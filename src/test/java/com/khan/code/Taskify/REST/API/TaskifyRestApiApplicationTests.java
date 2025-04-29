513d49228716440a609a8a1e5188212da517c126
abdulwariskhan <hellokhan.ch@gmail.com>
2025-04-29 11:09:29 +0200

    Initial commit

---
+++ src/test/java/com/khan/code/Taskify/REST/API/TaskifyRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.khan.code.Taskify.REST.API;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskifyRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
