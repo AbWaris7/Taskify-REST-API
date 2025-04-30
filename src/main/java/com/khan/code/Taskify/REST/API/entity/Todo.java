9ed118224fe3792602dd7e7416bd9621d31e13f7
abdulwariskhan <hellokhan.ch@gmail.com>
2025-04-30 07:49:04 +0200

    define ToDo entity with fields for task tracking

---
+++ src/main/java/com/khan/code/Taskify/REST/API/entity/Todo.java
@@ -0,0 +1,87 @@
+package com.khan.code.Taskify.REST.API.entity;
+
+import jakarta.persistence.*;
+
+@Table(name = "Todos")
+@Entity
+public class Todo {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(nullable = false)
+    private long id;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false)
+    private String description;
+
+    @Column(nullable = false)
+    private int priority;
+
+    @Column(nullable = false)
+    private boolean complete;
+
+    public Todo() {
+    }
+
+    public Todo(long id, String title, String description, int priority, boolean complete) {
+        this.id = id;
+        this.title = title;
+        this.description = description;
+        this.priority = priority;
+        this.complete = complete;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getPriority() {
+        return priority;
+    }
+
+    public void setPriority(int priority) {
+        this.priority = priority;
+    }
+
+    public boolean isComplete() {
+        return complete;
+    }
+
+    public void setComplete(boolean complete) {
+        this.complete = complete;
+    }
+
+    @Override
+    public String toString() {
+        return "Todo{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", description='" + description + '\'' +
+                ", priority=" + priority +
+                ", complete=" + complete +
+                '}';
+    }
+}
